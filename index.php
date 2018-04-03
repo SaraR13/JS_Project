a993aad6945128b0d313614608fdf3806c123680
SaraR13 <sara.ags.rodrigues@gmail.com>
2018-04-03 17:03:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!-- force fullscreen for apple devices -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <!-- safari status bar black / black-translucent -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <!-- avoid phone calls apple / blackberry -->
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="x-rim-auto-match" content="none">
+    <title>YOR - Your Own Reality</title>
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <link rel="shortcut icon" href="images/favicons/favicon.png" type="image/x-icon">
+    <link rel="icon" sizes="57x57" href="images/favicons/favicon-57x57.png">
+    <link rel="icon" sizes="72x72" href="images/favicons/favicon-72x72.png">
+    <link rel="icon" sizes="114x114" href="images/favicons/favicon-114x114.png">
+    <link rel="icon" sizes="144x144" href="images/favicons/favicon-144x144.png">
+</head>
+
+<body>
+  
+    <div class="bgImages" id="home">
+        <div class="wrap">
+            <div id="bgSlider">            
+                <div class="tiles">
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/first_tile_first_image.png"></div>
+                </div><!--/tiles-->
+  
+                <div class="tiles">
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_first_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_second_image.png"></div>
+                    <div class="tile" data-scale="1.1" data-image="images/second_tile_first_image.png"></div>
+                </div><!--/tiles-->
+  
+                <div class="tiles">
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_first_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_second_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_third_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_fourth_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_fifth_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_sixth_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_seventh_image.png"></div>
+                    <div class="tile last" data-scale="1.1" data-image="images/third_tile_eighth_image.png"></div>
+                </div><!--/tiles-->
+            </div><!--/bgSlider-->
+        </div><!--/wrap-->
+    </div><!--/bgImages-->  
+    
+<header>
+    <div class="container">
+        <div id="arrow-left" class="arrow"><img src="images/prev.png" alt="previous button" width="20" height="30"></div>
+        <div id="arrow-right" class="arrow"><img src="images/next.png" alt="next button" width="20" height="30"></div>
+        
+        <div class="mainMenu">
+            <div class="menuIcon">
+                <img id="menuBtn" src="images/menu.png" width="50" alt="sandwich menu icon">
+            </div><!--/menuIcon-->
+        
+            <div class="menu-container">
+                <ul>
+                    <div class="menu-item">
+                        <li><a class="menuLinks" href="#home">Home</a></li>
+                        <div class="menu-lt"></div>
+                        <div class="menu-rt"></div>
+                        <div class="menu-lb"></div>
+                        <div class="menu-rb"></div>
+                    </div><!--/menu-item-->
+
+                    <div class="menu-item">
+                        <li><a class="menuLinks" href="#about">About</a></li>
+                        <div class="menu-lt"></div>
+                        <div class="menu-rt"></div>
+                        <div class="menu-lb"></div>
+                        <div class="menu-rb"></div>
+                    </div><!--/menu-item-->
+
+                    <div class="menu-item">
+                        <li><a class="menuLinks" href="#gallery">Gallery</a></li>
+                        <div class="menu-lt"></div>
+                        <div class="menu-rt"></div>
+                        <div class="menu-lb"></div>
+                        <div class="menu-rb"></div>
+                    </div><!--/menu-item-->
+
+                    <div class="menu-item">
+                        <li><a class="menuLinks" href="#contact">Contact</a></li>
+                        <div class="menu-lt"></div>
+                        <div class="menu-rt"></div>
+                        <div class="menu-lb"></div>
+                        <div class="menu-rb"></div>
+                    </div><!--/menu-item--> 
+                </ul>
+            </div><!--/menu-container-->
+        </div><!--/mainMenu-->
+            
+        <div class="frameSmallDevices"></div>
+        <div class="welcomeSite">
+            <img src="images/logo.png" alt="logo">
+            <h1>Your Own Reality</h1>
+        </div><!--/welcomeSite--> 
+
+        <div class="socialMedia">
+            <a href="https://www.instagram.com/yor_your_own_reality/" class="logoInstagram" id="insta">Instagram</a>
+            <a href="https://www.facebook.com/YOReality2017/" class="logoFacebook" id="facebook">Facebook</a>
+            <a href="https://society6.com/yorownreality" class="logoSociety6" id="society6">Society6</a>
+        </div><!--/socialMedia-->
+   
+    </div><!--/container-->
+    
+    <div class="frameSmallDevices2"></div>
+    
+</header>
+
+<main>
+    <div class="container">
+        <div class="aboutPage" id="about">
+            <div class="aboutFrame" >
+                        <h2>About</h2>
+                        <p>The world is an inspiring place. Its beauty is all around us and, sometimes, even what at first sight seems boring and normal, when interpreted by individuals and worked on, can be transformed onto something new and beautiful. <br><br>The artwork I create always starts from a picture - places I visit, places I live in, work at, objects in my house, streets, anywhere. I then edit those photographs to give it an illustration look, and finally I repeat the motif to create a pattern. I end up creating patterns of day-to-day objects, things that are real and which I transform into something surreal, in mosaics. Of some photographs I only edit the colour, contrast and so on, and then repeat to create a surreal space composed by mirrored images.</p>
+            </div><!--/aboutFrame-->
+        </div><!--/aboutPage-->
+    </div><!--/container-->
+               
+    <div class="galleryPage" id="gallery">
+        <div class="containerGallery">
+            <h2>Gallery</h2>
+            <h3>1&#41; Surreal Patterns from Natural Landscapes</h3>
+            <div id="gallery1" class="photo-gallery">
+                <div class="current-photo">
+                    <img>
+                </div><!--/current-photo-->
+                <div class="photo-list">
+                    <ul>
+                        <li data-src="images/natural_landscapes1.jpg" alt="dakdkasjdksadad">
+                            <div class="thumbnail">
+                                <img src="images/natural_landscapes1.jpg" title="Fields from the plane" alt="natural landscapes image 1" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/natural_landscapes2.jpg">
+                            <div class="thumbnail">
+                                <img src="images/natural_landscapes2.jpg" title="fragmented landscape" alt="natural landscapes image 2" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/natural_landscapes3.jpg">
+                            <div class="thumbnail">
+                                <img src="images/natural_landscapes3.jpg" title="Inverted country side" alt="natural landscapes image 3" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/natural_landscapes4.jpg">
+                            <div class="thumbnail">
+                                <img src="images/natural_landscapes4.jpg" title="sinverted sea sight" alt="natural landscapes image 4" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/natural_landscapes5.jpg">
+                            <div class="thumbnail">
+                                <img src="images/natural_landscapes5.jpg" title="sea sight fractured" alt="natural landscapes image 5" />
+                            </div><!--/thumbnail-->
+                        </li>
+                    </ul>
+                </div><!--/photo-list-->
+            </div><!--/photo-gallery-->
+
+            <h3>2&#41; Surreal Patterns of People</h3>
+            <div id="gallery2" class="photo-gallery">
+                <div class="current-photo">
+                    <img>
+                </div><!--/current-photo-->
+                <div class="photo-list">
+                    <ul>
+                        <li data-src="images/patterns_of_people1.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people1.jpg" title="People being people" alt="patterns of people image 1" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_people2.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people2.jpg" title="kids in front of portal" alt="patterns of people image 2" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_people3.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people3.jpg" title="underground" alt="patterns of people image 3" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_people4.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people4.jpg" title="auntie and lua_2048_watermarked" alt="patterns of people image 4" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_people5.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people5.jpg" title="friends" alt="patterns of people image 5" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_people6.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_people6.jpg" title="Pink sunset surreal" alt="patterns of people image 6" />
+                            </div><!--/thumbnail-->
+                        </li>
+                    </ul>
+                </div><!--/photo-list-->
+            </div><!--/photo-gallery-->
+
+            <h3>3&#41; Patterns of Things</h3>
+            <div id="gallery3" class="photo-gallery">
+                <div class="current-photo">
+                    <img>
+                </div><!--/current-photo-->
+                <div class="photo-list">
+                    <ul>
+                        <li data-src="images/patterns_of_things1.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things1.jpg" title="Boats in the sky" alt="patterns of things image 1" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_things2.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things2.jpg" title="My old bedroom" alt="patterns of things image 2" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_things3.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things3.jpg" title="Stuff in my bedroom" alt="patterns of things image 3" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_things4.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things4.jpg" title="Studio days" alt="patterns of things image 4" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_things5.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things5.jpg" title="Tin soldier" alt="patterns of things image 5" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_things6.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_things6.jpg" title="Sangria glasses" alt="patterns of things image 6" />
+                            </div><!--/thumbnail-->
+                        </li>
+                    </ul>
+                </div><!--/photo-list-->
+            </div><!--/photo-gallery-->
+
+            <h3>4&#41; Patterns of Places</h3>
+            <div id="gallery4" class="photo-gallery">
+                <div class="current-photo">
+                    <img>
+                </div><!--/current-photo-->
+                <div class="photo-list">
+                    <ul>
+                        <li data-src="images/patterns_of_places1.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places1.jpg" title="Bar windows" alt="patterns of places image 1" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_places2.png">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places2.png" title="Big Ben" alt="patterns of places image 2" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_places3.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places3.jpg" title="English doors" alt="patterns of places image 3" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_places4.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places4.jpg" title="Haunted house or Old street station" alt="patterns of places image 4" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_places5.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places5.jpg" title="In the balcony" alt="patterns of places image 5" />
+                            </div><!--/thumbnail-->
+                        </li>
+                        <li data-src="images/patterns_of_places6.jpg">
+                            <div class="thumbnail">
+                                <img src="images/patterns_of_places6.jpg" title="Windows" alt="patterns of places image 6" />
+                            </div><!--/thumbnail-->
+                        </li>
+                    </ul>
+                </div><!--/photo-list-->
+            </div><!--/photo-gallery-->
+        </div><!--/containerGallery-->
+    </div><!--/galleryPage-->
+                  
+    <div class="container">        
+        <div class="contactPage" id="contact">
+            <div class="contactFrame">
+                <h2>Contact</h2>
+                <form id ="form_id" name="contactform" action="mail.php" method="post">
+                    <label>Name</label>
+                    <input name="name" placeholder="Type Here">
+                    
+                    <label>Email</label>
+                    <input name="email" type="email" placeholder="Type Here">
+                        
+                    <label>Message</label>
+                    <textarea class="userMsg" name="message" placeholder="Type Here"></textarea>
+                        
+                    <label>*What is 2+2? (Anti-spam)</label>
+                    <input name="human" placeholder="Type Here">
+                    
+                    <input id="submit" name="submit" type="submit" value="Send">
+                    
+                </form>
+            </div><!--/contactFrame-->
+        </div><!--/contact-->
+    </div><!--/container-->
+</main>
+        
+<footer>
+    <div class="container"><p>Designed by SARA RODRIGUES &copy; 2018</p></div><!--/container-->
+</footer>   
+
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>   
+<script src="js/lib/enquire/dist/enquire.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
+
---
+++ js/script.js
@@ -0,0 +1,219 @@
+/*BACKGROUND SLIDE*/
+
+(function(){
+
+let sliderImages = document.querySelectorAll(".tiles"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
+
+
+
+
+
+
+
+
+
+
+
+/*GALLERY*/
+    
+$(function() {
+
+	$('.photo-gallery ul li').click(function() {
+		var src = $(this).data('src');
+		var $gallery = $(this).closest('.photo-gallery');
+		$gallery.find('.current-photo img').attr('src', src);
+	});
+
+	$('.photo-gallery').each(function() {
+		$(this).find('li')[0].click();
+	});
+
+});
+
+
+
+
+    
+    
+/*MENU BORDER ANIMATION*/
+
+$(document).ready(documentReady);
+
+function documentReady()
+{
+    var button = $(".menu-container .menu-item li");
+
+    button.mouseover(onButtonOver);
+    function onButtonOver()
+    {
+
+    var parent = $(this).parent();
+        parent.addClass("active");
+
+        parent.find(".menu-lt").animate({marginLeft: 0, marginTop: 0, opacity: 1}, 100);
+
+        parent.find(".menu-rt").animate({marginRight: 0, marginTop: 0, opacity: 1}, 100);
+
+        parent.find(".menu-lb").animate({marginLeft: 0, marginTop: -5, opacity: 1}, 100);
+
+        parent.find(".menu-rb").animate({marginRight: 0, marginTop: -5, opacity: 1}, 100);
+
+    }
+
+    button.mouseout(onButtonOut);
+    function onButtonOut()
+    {
+
+    var parent = $(this).parent();
+        parent.removeClass("active");        
+
+        parent.find(".menu-lt").animate({marginLeft: -10, marginTop: -10, opacity: 0}, 100);
+
+        parent.find(".menu-rt").animate({marginRight: -10, marginTop: -10, opacity: 0}, 100);
+
+        parent.find(".menu-lb").animate({marginLeft: -10, marginTop: 5, opacity: 0}, 100);
+        
+        parent.find(".menu-rb").animate({marginRight: -10, marginTop: 5, opacity: 0}, 100);
+
+    }
+}
+
+
+
+
+
+
+
+
+/*BACKGROUND ZOOM ANIMATION*/
+    
+$('.tile')
+    
+    .on('mouseover', function(){
+      $(this).children('.photo').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
+    })
+    .on('mouseout', function(){
+      $(this).children('.photo').css({'transform': 'scale(1)'});
+    })
+    
+    
+    .each(function(){
+      $(this)
+        
+        .append('<div class="photo"></div>')
+        .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
+    })
+
+
+
+    const $menuLinks = $(".menuLinks");
+
+    function init(){
+        
+        $menuLinks.on("click", function(evt){
+            
+            evt.preventDefault();
+
+            let targetElement = $(this).attr("href");
+            
+            $("html, body")
+                .animate(
+                    {
+                        "scrollTop" : $(targetElement).offset().top 
+                    },
+                    1300
+
+                );
+
+
+        });
+    }
+
+    window.addEventListener("load", init);
+})();
+
+
+
+
+
+
+
+
+/*SCROLL DOWN ANIMATION*/
+
+(function () {
+    const
+        $menuBtn            =   $("#menuBtn"),
+        $topNavContainer    =   $(".mainMenu .menu-container")
+    ;
+
+    function bindMenuBtn() {
+        $menuBtn.on("click", function () {
+           $topNavContainer.slideToggle();
+        });
+    }
+    
+    function bindEnquire() {
+        enquire.register("screen and (min-width: 768px)", {
+
+          
+            match: function () {
+                $topNavContainer.show();
+            },
+
+            unmatch: function () {
+                $topNavContainer.hide();
+            }
+        });
+    } 
+
+    function init() {
+        bindEnquire();
+        bindMenuBtn();
+    } 
+
+    window.addEventListener("load", init);
+})();
